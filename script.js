59e7a827cb294cfa95fecbe2f5d167fe6b99cf46
samoreGE <samoreGE@hendrix.edu>
2019-11-01 20:41:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 var dadJokeURL = 'https://icanhazdadjoke.com/';
+makeRobot(3,"haha funny!");
 
 var newDiv = $("<div class=\"row joke-row\">");
 var roboKey = "asdf";
-var jokeKey = "haha funny!!";  
+var jokeKey = "haha funny!!";
 newDiv.html("\
 <div class=\"col-sm-3 robot-face\"><br \>\
 <img class=\"robot0\" src=\"https://avatars.dicebear.com/v2/bottts/:"+roboKey+".svg\"><br \>\
 </div><br \>\
 <div class=\"col-sm-9 joke-text\"><br \>\
 <div class=\"joke0\">" + jokeKey + "</div><br \>\
-</div><br \>\
-");
+</div>");
 // append prepend after
 $(".joke-robots").append(newDiv);
 
@@ -21,4 +21,16 @@ jokeRequest.send();
 jokeRequest.onload = function() {
 	var requestedJoke = jokeRequest.response;
 	$(".joke-robots").append(requestedJoke.joke);
+}
+
+function makeRobot(num, key){
+	var newDiv = $("<div class=\"row joke-row\">");
+	newDiv.html("\
+	<div class=\"col-sm-3 robot-face\"><br \>\
+	<img class=\"robot"+num+"\" src=\"https://avatars.dicebear.com/v2/bottts/:"+key+".svg\"><br \>\
+	</div><br \>\
+	<div class=\"col-sm-9 joke-text\"><br \>\
+	<div class=\"joke0\">" + key + "</div><br \>\
+	</div>");
+	$(".joke-robots").append(newDiv);
 }
