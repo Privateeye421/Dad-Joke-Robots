b1f131690e7f7430713bf5b3bc1371480a812153
samoreGE <samoreGE@hendrix.edu>
2019-11-01 21:22:32 -0500

    ITS ALIVE

--- script.js
+++ script.js
@@ -1,42 +1,37 @@
 var goadrichJokesURL = 'http://api.icndb.com/jokes/random?firstName=Mark&lastName=Goadrich';
-makeRobot(1,"haha funny!");
-rollJoke(1);
-var newDiv = $("<div class=\"row joke-row\">");
-var roboKey = "asdf";
-var jokeKey = "haha funny!!";
-newDiv.html("\
-<div class=\"col-sm-3 robot-face\"><br \>\
-<img class=\"robot0\" src=\"https://avatars.dicebear.com/v2/bottts/:"+roboKey+".svg\"><br \>\
-</div><br \>\
-<div class=\"col-sm-9 joke-text\"><br \>\
-<div class=\"joke0\">" + jokeKey + "</div><br \>\
-</div>");
-// append prepend after
-$(".joke-robots").append(newDiv);
+console.log("Hello world!");
 
+makeRobot(1,"haha funny!");
 
 function rollJoke(num) {
+	console.log("rollJoke("+num+")");
 	var jokeRequest=new XMLHttpRequest();
 	jokeRequest.open('GET', goadrichJokesURL);
 	jokeRequest.responseType = 'json';
 	jokeRequest.send();
 	jokeRequest.onload = function() {
 		var requestedJoke = jokeRequest.response;
-		var jokeID= requestedJoke.value.id;
+		console.log("requestedJoke="+jokeRequest.response);
+		var jokeID = requestedJoke.value.id;
+		console.log("jokeID="+jokeID);
 		var jokeText=requestedJoke.value.joke;
-		$(".joke-robots").append(requestedJoke.value.joke);
-		$("robot"+num).attr("src", "https://avatars.dicebear.com/v2/bottts/:asdf.svg")
+		console.log("jokeID="+jokeText);
+		var robotURL="https://avatars.dicebear.com/v2/bottts/:"+jokeID+".svg"
+		$(".robot"+num).attr("src", "https://avatars.dicebear.com/v2/bottts/:"+jokeID+".svg");
+		$(".joke"+num).html(jokeText);
 	}
 }
 
-function makeRobot(num, key){
+function makeRobot(num){
+	console.log("makeRobot("+num+")");
 	var newDiv = $("<div class=\"row joke-row\">");
 	newDiv.html("\
 	<div class=\"col-sm-3 robot-face\"><br \>\
-	<img class=\"robot"+num+"\" src=\"https://avatars.dicebear.com/v2/bottts/:"+key+".svg\"><br \>\
+	<img class=\"robot"+num+"\" src=\"https://avatars.dicebear.com/v2/bottts/:a.svg\"><br \>\
 	</div><br \>\
 	<div class=\"col-sm-9 joke-text\"><br \>\
-	<div class=\"joke0\">" + key + "</div><br \>\
+	<div class=\"joke"+num+"\">ERROR</div><br \>\
 	</div>");
 	$(".joke-robots").append(newDiv);
+	rollJoke(num);
 }
