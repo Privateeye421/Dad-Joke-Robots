c1cb524bf371a39da6d932e4820348d4163b1c4f
samoreGE <samoreGE@hendrix.edu>
2019-11-01 19:13:02 -0500

    Pushing so jack wont break things again

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-var newP = $("<p>");
-newP.html("The <strong>crocodiles</strong> have eaten this ENTIRE PAGE!");
-newP.addClass("crocodile");
+var newDiv = $("<div class=\"row joke-row\">");
+var roboKey = "asdf";
+var jokeKey = "haha funny!!";  
+newDiv.html("\
+<div class=\"col-sm-3 robot-face\"><br \>\
+<img class=\"robot0\" src=\"https://avatars.dicebear.com/v2/bottts/:"+roboKey+".svg\"><br \>\
+</div><br \>\
+<div class=\"col-sm-9 joke-text\"><br \>\
+<div class=\"joke0\">" + jokeKey + "</div><br \>\
+</div><br \>\
+");
 // append prepend after
-$("body").append(newP);
+$(".mainPage").append(newDiv);
+
+//<div class="col-sm-3 robot-face">
+//	<img class="robot0" src="https://avatars.dicebear.com/v2/bottts/:robot0.svg">
+//</div>
+//<div class="col-sm-9 joke-text">
+//	<div class="joke0">
+//		TEXT A:LKSJDFLK A:LKJSDLFKJ A: V2
+//	</div>
+//</div>
